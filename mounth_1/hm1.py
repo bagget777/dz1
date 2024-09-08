6035d588e8f7bcfd2228165bebea791865db9640
bagget777 <bagget7777@gmail.com>
2024-09-08 20:09:23 +0600

    first_hm

---
+++ mounth_1/hm1.py
@@ -0,0 +1,19 @@
+#hm1
+numb_1 = int(input('Введите первое число: '))
+numb_2 = int(input('Введите второе число: '))
+result = (numb_1 + numb_2)
+print(result)
+
+#hm2
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ') 
+age = input("Введите возраст: ") 
+text = f'Приветствуем вас {name} {surname},\nиз интересных фактов вам {age} лет.'
+print(text)
+
+#hm3
+x = float(input('Введите первое число: '))
+y = float(input('Введите второе число: '))
+z = float(input('Введите третье число: '))
+draw = (x + y) * z
+print(draw)
